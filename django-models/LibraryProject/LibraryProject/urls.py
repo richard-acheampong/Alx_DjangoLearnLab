103923c8e6f79307c36623c97d82b4f316007c11
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-01 15:43:06 +0000

    views and url config

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from relationship_app.views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def list_books(request):
+    """Retrieves all books and renders a template displaying the list."""
+    books = Book.objects.all()  # Fetch all book instances from the database
+    context = {'books': books}  # Create a context dictionary with book list
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+    
+    def get_context_data(self, **kwargs):
+        """Extend context to include books in the library."""
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()  # Fetch all books related to the library
+        return context
+   
