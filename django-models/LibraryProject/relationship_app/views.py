fb4547a52f36adbe096d08da1f30510e4590640f
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-01 17:59:56 +0000

    modified views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -51,16 +51,16 @@ def is_member(user):
 # Admin view - only accessible by Admin users
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 # Librarian view - only accessible by Librarian users
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 # Member view - only accessible by Member users
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
 
  
