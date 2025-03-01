7460e1cb01f64f11fa310e0d11c32767657fb2fa
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-01 16:20:46 +0000

    modified urls.py)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path("register/", register, name="register"),
     path("login/", LoginView.as_view(template_name="authentication/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(template_name="logout")),
     
 ]
