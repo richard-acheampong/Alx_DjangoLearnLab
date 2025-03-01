b168338beb8d594fd50cee450c24d3bf203e6857
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-01 17:11:12 +0000

    modified views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,27 +37,23 @@ def register(request):
     return render(request, "relationship_app/register.html", {"form": form})  
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-# Admin view - only accessible by Admin users
 @user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Welcome, Admin!")
+    return render(request, 'relationship_app/admin_view.html')
 
-# Librarian view - only accessible by Librarian users
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return HttpResponse("Welcome, Librarian!")
+    return render(request, 'relationship_app/librarian_view.html')
 
-# Member view - only accessible by Member users
 @user_passes_test(is_member)
 def member_view(request):
-    return HttpResponse("Welcome, Member!")
+    return render(request, 'relationship_app/member_view.html')
 
- 
