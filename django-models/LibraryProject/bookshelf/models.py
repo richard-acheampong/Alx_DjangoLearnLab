473281f6e606f907cdd3ab6ce560400bd90eab60
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-07 17:21:52 +0000

    Updated models.py

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -25,7 +25,11 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(null=True, blank=True)
 
-    
+    from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission, Group
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.username
