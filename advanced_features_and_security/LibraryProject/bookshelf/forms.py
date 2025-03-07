669ef0b6935edc3e7742febaa6c26f920be81913
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-07 18:06:25 +0000

    Modified forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -12,3 +12,16 @@ class BookForm(forms.ModelForm):
             'isbn': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter ISBN'}),
             'summary': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter summary'}),
         }
+
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100, 
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your message'})
+    )
