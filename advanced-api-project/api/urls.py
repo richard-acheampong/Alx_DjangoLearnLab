ece873ce9e90d8d469eaee1ea50807a29637b884
richard-acheampong <richardkaacheampong@gmail.com>
2025-03-14 15:10:29 +0000

    Modified book/update/<int:pk>/ to books/update/<int:pk>

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('books/', view= BookListView.as_view(), name= 'book-list'),
     path('books/<int:pk>/', view= BookDetailView.as_view(), name='book-detail')
     path('books/create/', view= BookCreateView.as_view(), name= 'book-create')
-    path('book/update/<int:pk>/', view= BookUpdateView.as_view(), name= 'book-update')
+    path('books/update/<int:pk>/', view= BookUpdateView.as_view(), name= 'book-update')
     path('books/delete/<int:pk>/', view= BookDeleteView.as_view(), name= 'book-delete')
 ]
